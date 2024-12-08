d067442025587f779abc8f737e551c785af87db6
ritaban06 <ghoshritaban2006@gmail.com>
2024-12-08 17:42:34 +0530

    Added pinata service

--- src/pages/CredentialUpload.jsx
+++ src/pages/CredentialUpload.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { useWeb3 } from '../contexts/Web3Context'
-import { ipfsService } from '../services/ipfsService'
+import { pinataService } from '../services/pinataService'
 import { ethers } from 'ethers'
 
 function CredentialUpload() {
