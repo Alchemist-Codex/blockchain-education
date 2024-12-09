068daf8abe5d7aa6ab6c401c5d95c308b6402f60
ritaban06 <ghoshritaban2006@gmail.com>
2024-12-09 17:27:16 +0530

    Docker

---
+++ src/config/index.js
@@ -0,0 +1,8 @@
+const config = {
+    rpcUrl: 'http://localhost:7545',
+    networkId: 1337,
+    networkName: 'Ganache',
+    contractAddress: import.meta.env.VITE_CONTRACT_ADDRESS,
+  };
+  
+  export default config;
