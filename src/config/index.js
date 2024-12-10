74e326f1efa631ec5818c189f43766f66188e9ea
ritaban06 <ghoshritaban2006@gmail.com>
2024-12-10 18:26:55 +0530

    Added comments to index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,9 @@
+/**
+ * Application Configuration
+ * Contains blockchain and network settings
+ */
 const config = {
+    // Local blockchain RPC endpoint (Ganache default)
     rpcUrl: 'http://localhost:7545',
     networkId: 1337,
     networkName: 'Ganache',
