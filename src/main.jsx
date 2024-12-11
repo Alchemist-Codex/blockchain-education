5142bbcc031e96dc0bdd80656cb4ecf06c1329e9
ritaban06 <ghoshritaban2006@gmail.com>
2024-12-11 19:18:54 +0530

    Enabled strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
+import { ThemeProvider } from './components/ThemeProvider'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
 )
-
