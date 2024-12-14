85fecb366342447a5fc796f32a3f50165be46139
ritaban06 <ghoshritaban2006@gmail.com>
2024-12-14 23:55:38 +0530

    Fixed reload error

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import { ThemeProvider } from './components/ThemeProvider'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
