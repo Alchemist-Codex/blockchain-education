0ee3fb0e20cade66d2388e697a4992b87fc96366
ritaban06 <ghoshritaban2006@gmail.com>
2024-12-15 00:05:46 +0530

    Fix 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { ThemeProvider } from './components/ThemeProvider'
@@ -11,11 +10,9 @@ const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
-        <Toaster position="top-right" />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider>
+      <Toaster position="top-right" />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
